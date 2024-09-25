6a7dacec6bbe1d037432d6f2c60197fef2531449
brandaoplaster <brandaoplaster@gmail.com>
2024-09-25 20:40:03 -0300

    feat: Adding create task function

--- src/services.rs
+++ src/services.rs
@@ -1,6 +1,7 @@
+use crate::{model::TaskModel, schema::CreateTaskSchema, AppState};
 use actix_web::{
-  get,
-  web::{scope, ServiceConfig},
+  get, post,
+  web::{scope, Data, Json, ServiceConfig},
   HttpResponse, Responder,
 };
 use serde_json::json;
@@ -12,7 +13,37 @@ async fn health_checker() -> impl Responder {
       "message": MESSAGE
   }))
 }
+
+#[post("/task")]
+async fn create_task(body: Json<CreateTaskSchema>, data: Data<AppState>) -> impl Responder {
+  match sqlx::query_as!(
+    TaskModel,
+    "INSERT INTO tasks (title, content) VALUES ($1, $2)
+            RETURNING * ",
+    body.title.to_string(),
+    body.content.to_string()
+  )
+  .fetch_one(&data.db)
+  .await
+  {
+    Ok(task) => {
+      let note_response = json!({
+          "status": "success",
+          "task": json!({
+              "task": task,
+          })
+      });
+      return HttpResponse::Ok().json(note_response);
+    }
+    Err(error) => {
+      return HttpResponse::InternalServerError().json(json!({
+          "status": "error",
+          "message": format!("{:?}", error)
+      }))
+    }
+  }
+}
 pub fn config(conf: &mut ServiceConfig) {
-  let scope = scope("/api").service(health_checker);
+  let scope = scope("/api").service(health_checker).service(create_task);
   conf.service(scope);
 }
