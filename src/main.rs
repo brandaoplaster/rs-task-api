9849cb0b39dcb158721a18cd4ab036273a3ac5a3
brandaoplaster <brandaoplaster@gmail.com>
2024-09-24 22:30:22 -0300

    feat: Adding database config

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 mod model;
 mod schema;
 mod services;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 
+use dotenv::dotenv;
+
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+pub struct AppState {
+  db: Pool<Postgres>,
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   println!("Server started successfully");
-  HttpServer::new(move || App::new().configure(services::config))
-    .bind(("127.0.0.1", 8080))?
-    .run()
+
+  dotenv().ok();
+
+  let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must to be set");
+  let pool = match PgPoolOptions::new()
+    .max_connections(10)
+    .connect(&database_url)
     .await
+  {
+    Ok(pool) => {
+      println!("Connetion DB resolved");
+      pool
+    }
+    Err(error) => {
+      println!("Failed to connect to the dabase: {:?}", error);
+      std::process::exit(1)
+    }
+  };
+
+  HttpServer::new(move || {
+    App::new()
+      .app_data(web::Data::new(AppState { db: pool.clone() }))
+      .configure(services::config)
+  })
+  .bind(("127.0.0.1", 8080))?
+  .run()
+  .await
 }
